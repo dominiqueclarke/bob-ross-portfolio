ceb9b6f631ca608e0edb163cc59ac382d926c8b8
Dominique Clarke <dominiqueclarke@Dominiques-MacBook-Pro.local>
2020-10-06 22:33:34 -0400

    add art assets

---
+++ src/constants.js
@@ -0,0 +1,43 @@
+export const ART = [
+    {
+        asset: "background1.jpg",
+        artistName: "Europeana",
+        artistLink: "https://unsplash.com/@europeana",
+        accentColors: ["#672221", "#022960"],
+    },
+    {
+        asset: "background2.jpg",
+        artistName: "Steve Johnson",
+        artistLink: "https://unsplash.com/@steve_j",
+    },
+    {
+        asset: "background3.jpg",
+        artistName: "Daniel Olah",
+        artistLink: "https://unsplash.com/@danesduet",
+    },
+    {
+        asset: "background4.jpg",
+        artistName: "Orlova Maria",
+        artistLink: "https://unsplash.com/@orlovamaria",
+    },
+    {
+        asset: "background5.jpg",
+        artistName: "Geordanna Cordero",
+        artistLink: "https://unsplash.com/@geordannatheartist",
+    },
+    {
+        asset: "background6.jpg",
+        artistName: "Europeana",
+        artistLink: "https://unsplash.com/@europeana",
+    },
+    {
+        asset: "background7.jpg",
+        artistName: "Europeana",
+        artistLink: "https://unsplash.com/@europeana",
+    },
+    {
+        asset: "background8.jpg",
+        artistName: "Geordanna Cordero",
+        artistLink: "https://unsplash.com/@geordannatheartist",
+    },
+];
